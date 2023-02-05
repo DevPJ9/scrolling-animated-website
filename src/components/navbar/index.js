2c8c4845f7ccefe0611b6aa28dd88fbdb2ccaacb
Pranjal <pranjal9.joshi@gmail.com>
2023-02-05 22:52:10 +0530

    adding the first commit and the project's basic structure to github

---
+++ src/components/navbar/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Nav } from "./NavbarElements";
+
+// we named this folder index.js so that when we import this class we can just import till the folder name and it will autiomatically search index.js file
+
+const Navbar = () => {
+  return (
+    <>
+      <Nav>
+        {/* <NavbarContainer>
+          <NavLogo>dolla</NavLogo>
+        </NavbarContainer> */}
+      </Nav>
+    </>
+  );
+};
+
+export default Navbar;
